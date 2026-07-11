e6cc1a45e01e5e303dc5c282011c87925ae3e6c8
Yevgeniy343 <rt4387@yahoo.com>
2026-07-11 11:44:44 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
+#include "src/Commands.h"
 #include "src/Device.h"
 
 #include <iostream>
 
 int main() {
-
     Device device;
 
     if (!device.open()) {
         return 1;
     }
 
-    std::cout << "Everything OK\n";
+    const auto startData =
+        HantekCommands::startSampling();
+
+    const bool started = device.controlWrite(
+        HantekCommands::START_SAMPLING,
+        0,
+        0,
+        startData.data(),
+        static_cast<std::uint16_t>(startData.size())
+    );
+
+    if (!started) {
+        std::cerr << "Failed to start sampling\n";
+        return 1;
+    }
+
+    std::cout << "Sampling started\n";
+
+    const auto stopData =
+        HantekCommands::stopSampling();
+
+    const bool stopped = device.controlWrite(
+        HantekCommands::START_SAMPLING,
+        0,
+        0,
+        stopData.data(),
+        static_cast<std::uint16_t>(stopData.size())
+    );
+
+    if (!stopped) {
+        std::cerr << "Failed to stop sampling\n";
+        return 1;
+    }
+
+    std::cout << "Sampling stopped\n";
 
     return 0;
 }
